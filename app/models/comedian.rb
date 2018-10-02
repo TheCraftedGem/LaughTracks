35523008d78f1bce4eef6f2c90c2bb61c487a7ba
Millstone <millstone@Millstones-MacBook-Pro.local>
2018-10-01 20:46:22 -0600

    woof

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,2 +1,10 @@
 class Comedian < ActiveRecord::Base
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :city, presence: true
+  has_many :specials
+
+  def self.average_age
+    average(:age)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
+require 'CSV'
+require 'pry'
+require_relative '../app/models/comedian'
+require_relative '../app/models/special'
+# require_relative './comedians.csv'
 
+
+  def self.load(filepath)
+    csv_object = CSV.foreach(filepath, headers: true, header_converters: :symbol)
+      Comedian.create(name = comedian[:name], age = comedian[:age], city = comedian[:city])
+      binding.pry 
+  end
+
+  self.load('db/comedians.csv')
